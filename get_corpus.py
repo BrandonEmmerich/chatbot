75891dee153d2beb18a064529429ca374b38c588
Brandon Emmerich <b@Brandons-Air.fios-router.home>
2019-03-28 21:44:01 -0400

    Generalize Corpus to All Presidents
    Generalize the corpus to all presidents so that the chat bot
    can be a presidents chat bot, not just an Obama chat bot.

--- get_corpus.py
+++ get_corpus.py
@@ -5,39 +5,56 @@ import re
 import requests
 import unidecode
 
-url_wiki_obama = 'https://en.wikipedia.org/wiki/Barack_Obama'
+url_wiki_us_presidents = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States'
 
 XPATH_TEXT = "//div[@class='mw-parser-output']/p//text()"
-
+XPATH_PRESIDENT_LINKS = "//table[@class='wikitable']/tbody/tr/td[4]/b/big/a/@href"
 
 def make_request_with_xpath(url, xpath):
     response = requests.get(url)
     root = etree.HTML(response.content)
-    content = root.xpath(xpath)
+    raw_text = root.xpath(xpath)
 
-    return content
+    return raw_text
 
-def clean_string(raw_text):
+def get_wiki_urls():
+    hrefs = make_request_with_xpath(url_wiki_us_presidents, XPATH_PRESIDENT_LINKS)
+    wiki_urls = ['https://en.wikipedia.org' + suffix for suffix in hrefs]
+
+    return wiki_urls
+
+def raw_text_to_clean_string(raw_text):
     big_string = ''.join(raw_text).replace("\'","'")
     clean_string = unidecode.unidecode(re.sub('\[.+\]','',big_string))
 
     return clean_string
 
-def parse_corpus(clean_string):
+def parse_corpus(clean_string, url):
     sentence_tokens = nltk.sent_tokenize(clean_string)
-    word_tokens = nltk.word_tokenize(clean_string)
 
     data = {
+        'url': url,
         'sentence_tokens': sentence_tokens,
-        'word_tokens': word_tokens,
     }
 
     return data
 
 if __name__ == '__main__':
-    raw_text = make_request_with_xpath(url_wiki_obama, XPATH_TEXT)
-    clean_string = clean_string(raw_text)
-    data = parse_corpus(clean_string)
+    wiki_urls = get_wiki_urls()
+    president_wikis = []
 
-    with open('data/obama.json', 'w') as outfile:
-        json.dump(data, outfile)
+    for url in wiki_urls:
+        print(url)
+
+        try:
+            raw_text = make_request_with_xpath(url=url, xpath=XPATH_TEXT)
+            clean_string = raw_text_to_clean_string(raw_text)
+            data = parse_corpus(clean_string=clean_string, url=url)
+            president_wikis.append(data)
+        except Exception as e:
+            print(e)
+
+    total_data = {'data': president_wikis}
+
+    with open('data/presidents.json', 'w') as outfile:
+        json.dump(total_data, outfile)
